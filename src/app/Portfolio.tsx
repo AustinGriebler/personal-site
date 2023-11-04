7de3f381d75e4453c14561a4e8a20abae10dce32
Austin Griebler <austingriebler@gmail.com>
2023-11-04 10:04:26 -0500

    move asset folder

--- src/app/Portfolio.tsx
+++ src/app/Portfolio.tsx
@@ -10,42 +10,42 @@ const Portfolio = () => {
       >
         <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 items-center justify-center gap-4 [&amp;>div]:w-full p-8">
           <PortfolioCard
-            imageLink="/src/assets/familypicture.jpg"
+            imageLink="../../public/assets/familypicture.jpg"
             projectName="luuke"
             projectCompany="Personal Project"
             projectLink="/"
             languages={["React", "Other", "Things"]}
           />
           <PortfolioCard
-            imageLink="/src/assets/bananaboxes.png"
+            imageLink="../../public/assets/bananaboxes.png"
             projectName="Banana Box"
             projectCompany="Bullseye Media"
             projectLink="https://bananaboxes.com"
             languages={["PHP", "WordPress", "JavaScript"]}
           />
           <PortfolioCard
-            imageLink="/src/assets/onesta.png"
+            imageLink="../../public/assets/onesta.png"
             projectName="Onesta"
             projectCompany="Bullseye Media"
             projectLink="https://onestahaircare.com"
             languages={["PHP", "WordPress", "JavaScript"]}
           />
           <PortfolioCard
-            imageLink="/src/assets/blarutan.png"
+            imageLink="../../public/assets/blarutan.png"
             projectName="B'Larutan"
             projectCompany="Bullseye Media"
             projectLink="https://blarutan.com"
             languages={["PHP", "WordPress", "JavaScript"]}
           />
           <PortfolioCard
-            imageLink="/src/assets/dustys.png"
+            imageLink="../../public/assets/dustys.png"
             projectName="Dusty's Car Wash"
             projectCompany="Bullseye Media"
             projectLink="https://dustyswash.com"
             languages={["PHP", "WordPress", "JavaScript"]}
           />
           <PortfolioCard
-            imageLink="/src/assets/talentspark.png"
+            imageLink="../../public/assets/talentspark.png"
             projectName="Talent Spark"
             projectCompany="Bullseye Media"
             projectLink="https://talentspark.com"
